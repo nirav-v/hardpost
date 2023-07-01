778b7bb9ee5603089fbfecf246092e4467524584
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-01 12:10:21 -0700

    move db credentials to .env file - nothing sensitive, just best practice

--- util/database.js
+++ util/database.js
@@ -1,9 +1,15 @@
 // import sequelize and set up a connection pool to the mysql database
 const Sequelize = require("sequelize");
+require("dotenv").config(); // configure package for loading local env file
 
-const sequelize = new Sequelize("hardpost_db", "root", "mysql123", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
 
 module.exports = sequelize;
