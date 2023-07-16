1af2902b7fb935f2f3247d192a5573dfbaae3c3d
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-16 13:56:26 -0700

    conditionally rendering login and signup forms based on loggedIn state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import ShopPage from "./components/pages/ShopPage";
 import SignUpForm from "./components/forms/SignUpForm";
 import LoginForm from "./components/forms/LoginForm";
+import AddItemForm from "./components/forms/AddItemForm1";
 
 import { useState } from "react";
 
@@ -8,10 +9,20 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   console.log("loggedIn ", loggedIn);
 
+  // conditionally render login and sign up forms based on loggedIn state
+  let loginForm;
+  let signUpForm;
+  if (!loggedIn) {
+    loginForm = <LoginForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} />;
+    signUpForm = <SignUpForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} />;
+  }
+
   return (
     <div>
-      <SignUpForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      <LoginForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      {loggedIn ? <p>logged in</p> : null}
+      {loginForm}
+      {signUpForm}
+      <AddItemForm />
       <ShopPage />;
     </div>
   );
