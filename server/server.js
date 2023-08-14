f68682f3f404d3db238a07aa30add62f698c3da5
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-08-14 14:50:53 -0700

    had to add express static middleware for serving client side code in dist build directly in order to serve static files from images folder

--- server/server.js
+++ server/server.js
@@ -16,9 +16,14 @@ const port = process.env.PORT || 3000;
 const apiRoutes = require("./controllers/api");
 const shopRoutes = require("./controllers/shop-routes");
 
+app.use(express.static("dist"));
+
 app.use(cors()); //allow for client side requests without getting CORS error
 app.use(bodyParser.urlencoded({ extended: true })); // to parse incoming req body
 app.use(express.static(path.join(__dirname, "public")));
+// serving files from images folder
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use(express.json()); // needed to send json req.body in insomnia post requests
 
 // Create a new sequelize store using the express-session package
@@ -45,7 +50,9 @@ app.use("/api", apiRoutes);
 app.use(shopRoutes);
 
 app.use("*", (req, res, next) =>
-  res.send("navigate to one of the existing routes in your controller")
+  // in deployment we send index.html for all additional paths not defined by our express routes
+  // react router pushes different paths to window url
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
 );
 
 // see available magic methods on User instances based on the model associations we defined
