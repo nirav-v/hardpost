f4dd5352c48b0cd7e4200730394bb53fc8f3ce62
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-08-27 12:30:59 -0700

    implement loading bar component from react-loader-spinner package on addItem form submit. Correctly renders load bar or form based on loading state being true or false

--- client/src/components/forms/AddItemForm1.jsx
+++ client/src/components/forms/AddItemForm1.jsx
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, Fragment } from "react";
+import { ProgressBar } from "react-loader-spinner";
 
 function addItemForm() {
   const [name, setName] = useState("");
@@ -53,58 +54,74 @@ function addItemForm() {
   };
 
   return (
-    <div>
-      {loading ? <h1>LOADING....</h1> : null}
-      <p>addItemForm</p>
-      <form onSubmit={handleSubmit}>
-        {/* inputs for item name, price category, description */}
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-        <label>
-          Category:
-          <select
-            value={category}
-            onChange={(event) => setCategory(event.target.value)}>
-            <option value="decks">decks</option>
-            <option value="shoes">shoes</option>
-            <option value="trucks">trucks</option>
-            <option value="wheels">wheels</option>
-            <option value="pants">pants</option>
-            <option value="other">other</option>
-          </select>
-        </label>
-        <label>Price</label>
-        <input
-          type="number"
-          step="any"
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        />
-        <label>Description</label>
-        <input
-          type="text"
-          description="name"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-        <label> Image</label>
-        {/* from react docs for file inputs: In React, an <input type="file" /> is always an uncontrolled component because its value can only be set by a user, and not programmatically. */}
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          defaultValue=""
-          ref={imageInput}
-        />
-        <input type="submit" />
-      </form>
-    </div>
+    <Fragment>
+      {loading ? (
+        <div>
+          <h3>Uploading your item - please do not refresh or close the page</h3>
+          <ProgressBar
+            height="80"
+            width="80"
+            ariaLabel="progress-bar-loading"
+            wrapperStyle={{}}
+            wrapperClass="progress-bar-wrapper"
+            borderColor="#F4442E"
+            barColor="#51E5FF"
+          />
+        </div>
+      ) : (
+        <div>
+          <p>addItemForm</p>
+          <form onSubmit={handleSubmit}>
+            {/* inputs for item name, price category, description */}
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <label>
+              Category:
+              <select
+                value={category}
+                onChange={(event) => setCategory(event.target.value)}>
+                <option value="decks">decks</option>
+                <option value="shoes">shoes</option>
+                <option value="trucks">trucks</option>
+                <option value="wheels">wheels</option>
+                <option value="pants">pants</option>
+                <option value="other">other</option>
+              </select>
+            </label>
+            <label>Price</label>
+            <input
+              type="number"
+              step="any"
+              value={price}
+              onChange={(event) => setPrice(event.target.value)}
+            />
+            <label>Description</label>
+            <input
+              type="text"
+              description="name"
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
+            />
+            <label> Image</label>
+            {/* from react docs for file inputs: In React, an <input type="file" /> is always an uncontrolled component because its value can only be set by a user, and not programmatically. */}
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              defaultValue=""
+              ref={imageInput}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
