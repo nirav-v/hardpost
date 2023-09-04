737c7e91f8332aaaaff55de37bb7794ed582f8ce
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-09-04 16:28:21 -0700

    install Chakra UI and wrap App in Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import "./styles/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
