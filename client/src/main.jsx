913ceffddcd11b8667ed0c6e2d2cbb589f0cb442
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-09-08 15:07:55 -0700

    add config setup to set default color mode to dark

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,22 @@ import "./styles/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider them={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
