3b1c1ce9304b6d58866b5eb6c24b784486d54c91
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-09-15 15:56:35 -0700

    style/formatting of order form and timestamp util function

--- client/src/pages/OrdersPage.jsx
+++ client/src/pages/OrdersPage.jsx
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Auth from "../util/auth";
+import {
+  List,
+  ListItem,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Box,
+  Stack,
+  StackDivider,
+} from "@chakra-ui/react";
+import formatTimestamp from "../util/formatTimestamp";
 
 function OrdersPage() {
   const [orders, setOrders] = useState([]);
@@ -15,22 +29,56 @@ function OrdersPage() {
 
   console.log(orders);
 
+  // function to calculate the total price from an order object
+  const orderPrice = (order) =>
+    order.items.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.price,
+      0
+    );
+
   return (
     <div>
       <h2>your orders</h2>
       {orders.length ? (
-        <ul>
+        <List spacing={5}>
           {orders.map((order) => {
             return (
-              <li key={order.id}>
-                <p>placed: {order.createdAt}</p>
-                {order.items.map((item) => (
-                  <p key={item.id}>{item.name}</p>
-                ))}
-              </li>
+              <ListItem key={order.id}>
+                <Card>
+                  <CardHeader>
+                    <Heading size="md">
+                      {formatTimestamp(order.createdAt)}
+                    </Heading>
+                  </CardHeader>
+
+                  <CardBody>
+                    <Stack divider={<StackDivider />} spacing="4">
+                      <Box>
+                        <Heading size="xs">
+                          {order.items.length} Item
+                          {order.items.length > 1 ? "s" : ""} Ordered
+                        </Heading>
+                        {order.items.map((item) => (
+                          <Text key={item.id} pt="2" fontSize="sm">
+                            {item.name}
+                          </Text>
+                        ))}
+                      </Box>
+                      <Box>
+                        <Heading size="xs" textTransform="uppercase">
+                          Total Price
+                        </Heading>
+                        <Text pt="2" fontSize="sm">
+                          ${orderPrice(order)}
+                        </Text>
+                      </Box>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </ListItem>
             );
           })}
-        </ul>
+        </List>
       ) : (
         "no orders"
       )}
---
+++ client/src/util/formatTimestamp.js
@@ -0,0 +1,13 @@
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const year = date.getFullYear().toString().slice(-2); // Get last two digits of the year
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = (date.getHours() % 12 || 12).toString().padStart(2, "0"); // Convert to 12-hour format
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const ampm = date.getHours() >= 12 ? "pm" : "am"; // Determine AM/PM
+
+  return `${month}/${day}/${year} ${hours}:${minutes} ${ampm}`;
+}
+
+export default formatTimestamp;
