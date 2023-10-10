43d5bf71a492f86cc06fa8146af45b5859ab1a82
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-10 16:55:16 -0700

    refactor util file to typescript

--- client/src/util/cartApi.ts
+++ client/src/util/cartApi.ts
@@ -1,6 +1,6 @@
 import Auth from "./auth";
 
-const addCartItem = async (itemId) => {
+const addCartItem = async (itemId: number) => {
   const res = await fetch("/api/cart", {
     method: "POST",
     body: JSON.stringify({ itemId }),
@@ -12,7 +12,7 @@ const addCartItem = async (itemId) => {
   return res.json();
 };
 
-const deleteCartItem = async (itemId) => {
+const deleteCartItem = async (itemId: number) => {
   const res = await fetch("/api/cart/delete-item", {
     method: "POST",
     body: JSON.stringify({ itemId }),
