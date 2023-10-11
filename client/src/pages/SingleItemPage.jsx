7495bcaac836f433b7457934c43fc3b03294042b
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-11 15:03:56 -0700

    fix log in bug from copied conditional button rendering code in single item page

--- client/src/pages/SingleItemPage.jsx
+++ client/src/pages/SingleItemPage.jsx
@@ -1,6 +1,8 @@
 import Auth from "../util/auth";
+import ButtonModal from "../components/UI/ButtonModal";
 import { addCartItem } from "../util/cartApi";
 import React from "react";
+import { useState } from "react";
 import { useItemsContext } from "../context/ItemsContext";
 import { useCartContext } from "../context/CartContext";
 import { useParams } from "react-router-dom";
@@ -25,9 +27,18 @@ const SingleItemPage = () => {
   const [cart, setCart] = useCartContext();
 
   // DUPLICATE CODE from ProductCard
+  // access the current user's id by decoding the jwt in local storage
+  let userId;
+  if (Auth.isLoggedIn()) {
+    userId = Auth.getPayload().userId;
+    console.log("userId: ", userId);
+  }
   // create a set of cartIds to lookup when mapping over items below
   const cartIds = new Set();
   cart.forEach((cartItem) => cartIds.add(cartItem.id));
+
+  // loading state to track while add to cart request is happening and finished
+  const [loading, setLoading] = useState(false);
 
   const params = useParams();
 
@@ -37,10 +48,49 @@ const SingleItemPage = () => {
   const item = items.filter((item) => item.id === parseInt(params.itemId))[0];
 
   const handleAddCartClick = async (itemId) => {
-    if (!Auth.isLoggedIn) return;
+    if (!Auth.isLoggedIn()) return;
     const updatedItems = await addCartItem(itemId);
     setCart(updatedItems);
   };
+
+  // DUPLICATE CODE from ProductCard for conditional rendering of button, extract somewhere else for reusability
+  // rendering a different button under different conditions
+  let button;
+
+  if (item.sold) {
+    button = <Text>Sold</Text>; // item is sold, render "sold" button
+  } else if (!Auth.isLoggedIn()) {
+    // user not logged in, render the modal opening button to tell them to log in
+    button = (
+      <ButtonModal buttonContent="Add to Cart">
+        {" "}
+        <Text fontSize="lg" align="center">
+          You must have an account and be logged in to purchase this item
+        </Text>
+      </ButtonModal>
+    );
+  } else if (item.userId === userId) {
+    button = <Text>My item</Text>; // item belongs to logged in user, render "my item" button
+  } else if (cartIds.has(item.id)) {
+    button = (
+      <Button
+        onClick={() => handleCartDelete(item.id)}
+        colorScheme="red"
+        width="full">
+        Remove from cart
+      </Button>
+    ); // item is in cart, return "remove from cart button"
+  } else if (!cartIds.has(item.id)) {
+    // item is not in cart, return "add to cart button"
+    button = (
+      <Button
+        onClick={() => handleAddCartClick(item.id)}
+        colorScheme="blue"
+        width="full">
+        {loading ? "adding to your cart..." : "Add to cart"}
+      </Button>
+    );
+  }
 
   return (
     <Center>
@@ -61,21 +111,7 @@ const SingleItemPage = () => {
         </CardBody>
         <Divider />
         <CardFooter>
-          <ButtonGroup spacing="2">
-            {/* <Button variant="solid" colorScheme="blue">
-            Buy now
-          </Button> */}
-            {cartIds.has(item.id) ? (
-              "In your cart already"
-            ) : (
-              <Button
-                onClick={() => handleAddCartClick(item.id)}
-                variant="ghost"
-                colorScheme="blue">
-                Add to cart
-              </Button>
-            )}
-          </ButtonGroup>
+          <ButtonGroup spacing="2">{button}</ButtonGroup>
         </CardFooter>
       </Card>
     </Center>
