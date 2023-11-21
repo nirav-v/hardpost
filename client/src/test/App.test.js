82f48cfb25dd202a50e4a0601cfce3f643d2e921
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-11-21 13:46:27 -0800

    testing setup with jest and react testing library

---
+++ client/src/test/App.test.js
@@ -0,0 +1,9 @@
+import ButtonModal from "../components/modals/ButtonModal";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<ButtonModal />);
+  });
+});
