fd9835eff66eaa1412f4ef3e6522a9296a3ac332
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-24 14:31:38 -0800

    typed items context

--- client/src/context/ItemsContext.tsx
+++ client/src/context/ItemsContext.tsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
+import { Item } from "../types/ItemTypes";
 
-const ItemsContext = createContext();
+type ItemsContextType = [Item[], React.Dispatch<React.SetStateAction<Item[]>>];
+
+const ItemsContext = createContext<ItemsContextType>([[], () => {}]); // default values to be filled with items and setItems from useState
 
 const useItemsContext = () => useContext(ItemsContext);
-const ItemsProvider = ({ children }) => {
-  const [items, setItems] = useState([]);
+
+const ItemsProvider = ({ children }: { children: React.ReactNode }) => {
+  const [items, setItems] = useState<Item[]>([]);
 
   console.log(items);
   useEffect(() => {
     // Make the fetch request here
     fetch("/shop")
       .then((response) => response.json())
-      .then((items) => {
+      .then((items: Item[]) => {
         // sort items in place by available items first
         items.sort((item2, item1) => {
           if (!item2.sold && item1.sold) return -1;
+          return 0;
         });
 
         // set the global items state
