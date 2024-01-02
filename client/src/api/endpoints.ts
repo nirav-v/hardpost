6fbba20c886d738077b657a3a8d7ce8bdcb44682
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-01-01 18:43:29 -0800

    refactor endpoints config to include object with both url and fetch options

--- client/src/api/endpoints.ts
+++ client/src/api/endpoints.ts
@@ -1,4 +1,18 @@
+type LoginBody = { email: string; password: string };
+
 export const endpoints = {
-  login: "api/user/login",
+  login: {
+    url: "api/user/login",
+    options: (body: LoginBody) => {
+      return {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json", // tells server that data is in json format
+        },
+      };
+    },
+  },
+
   signup: "api/user/signup",
 };
--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -6,14 +6,9 @@ export const userApi = {
   signup: async () => {},
 
   login: async (body: LoginBody) => {
+    const { url, options } = endpoints.login;
     try {
-      const response = await fetch(endpoints.login, {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-          "Content-Type": "application/json", // tells server that data is in json format
-        },
-      });
+      const response = await fetch(url, options(body));
       if (response.status === 200) return response.json();
       else return null;
     } catch (error) {
