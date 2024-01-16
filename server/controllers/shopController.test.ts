35d08cb6232066ce4c691950939e3e45e1a8aa7d
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-01-16 11:34:02 -0800

    rm repetitive mocking of res object in shopController tests

--- server/controllers/shopController.test.ts
+++ server/controllers/shopController.test.ts
@@ -1,35 +1,30 @@
-import Item from "../models/Item.js";
-import { getItemById, getShopItems } from "./shopController.js";
-import { mockItems } from "./mockData";
+import Item from '../models/Item.js';
+import { getItemById, getShopItems } from './shopController.js';
+import { mockItems } from './mockData';
 
-describe("Item controller logic", () => {
-  test("getShopItems returns items in json format", async () => {
+describe('Item controller logic', () => {
+  const req = {};
+  const res = {
+    status: jest.fn().mockImplementation(statusCode => res),
+    json: jest.fn(),
+  };
+  test('getShopItems returns items in json format', async () => {
     // jest.fn() returns a mock function
 
     Item.findAll = jest.fn().mockResolvedValue(mockItems);
-
-    const req = {};
-    const res = {
-      status: jest.fn().mockImplementation(() => res),
-      json: jest.fn(),
-    };
 
     await getShopItems(req, res);
 
     expect(res.json).toHaveBeenCalledWith(mockItems);
   });
 
-  test("getITemById returns item in json format", async () => {
+  test('getITemById returns item in json format', async () => {
     const req = { params: { itemId: 1 } };
-    const res = {
-      status: jest.fn().mockImplementation(() => res),
-      json: jest.fn(),
-    };
 
     let foundItem;
     // mock the sequelize method's return value
-    Item.findByPk = jest.fn().mockImplementation((id) => {
-      foundItem = mockItems.find((item) => item.id === id);
+    Item.findByPk = jest.fn().mockImplementation(id => {
+      foundItem = mockItems.find(item => item.id === id);
       return new Promise((res, rej) => res(foundItem));
     });
 
