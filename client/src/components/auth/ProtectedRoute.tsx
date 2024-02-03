bf7bed44151249a73f59e809df0fa3e3294fc95f
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-03 09:52:32 -0800

    switch case to protected route component to dynamically chage message logged out user sees based on page they attempt to access

--- client/src/components/auth/ProtectedRoute.tsx
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -5,16 +5,36 @@ import { PropsWithChildren } from 'react';
 import LoginDisplay from '../UI/LoginDisplay';
 import { useUserContext } from '../../context/UserContext';
 import { Box, Container } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }: PropsWithChildren) {
   const [loggedIn, setLoggedIn] = useUserContext();
+
+  const location = useLocation();
+  const path = location.pathname;
+
+  console.log(path);
+
+  // switch the message user sees based on the page they attempt to view
+  let message;
+  switch (path) {
+    case '/add-item':
+      message = 'post an item';
+      break;
+    case '/user-items':
+      message = 'view the items you have posted';
+      break;
+    case '/orders':
+      message = 'view your order history';
+      break;
+  }
 
   if (!loggedIn) {
     return (
       <div>
         <Container maxW="2xl" centerContent>
           <Box paddingTop="4" maxW="md" fontSize={'25px'}>
-            Please log in to view this content
+            Please log in to {message}
           </Box>
           <LoginDisplay setLoggedIn={setLoggedIn} />;
         </Container>
