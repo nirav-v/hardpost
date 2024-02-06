17b7693e7b884d0e3ac4db3d601139339bb63ff8
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-06 14:49:52 -0800

    move color theme btn logout btn, and login btn modal into the navbar component. App.tsx looks very clean now - only the providers, navbar, and Outlet component for each page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import Auth from './util/auth';
 import NavBar from './components/UI/NavBar';
 import { Outlet } from 'react-router-dom';
-import { Fragment, useEffect, useState } from 'react';
-// import { ItemsProvider } from './context/ItemsContext';
-import LogoutButton from './components/buttons/LogoutButton';
-import { Button, Box, Container, useColorMode, Flex } from '@chakra-ui/react';
+import { Fragment, useEffect } from 'react';
+
 import CartProvider from './context/CartContext';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
