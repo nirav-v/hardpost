b34c9d10f37997631419a25012f7b4b3c4678907
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-16 09:11:01 -0800

    extract all item filtering logic into the filter checkbox component, ShopPage only responsible for fetching an rendering item data and passing it to filter checkbox as props

--- client/src/components/inputs/FilterCheckbox.tsx
+++ client/src/components/inputs/FilterCheckbox.tsx
@@ -1,21 +1,56 @@
-import { Checkbox, CheckboxGroup, Stack } from "@chakra-ui/react";
+import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react';
+import { Item } from '../../types/ItemTypes';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 type FilterCheckboxProps = {
-  onChange: (choices: string[]) => void;
-  defaultValue: string[];
+  itemData: Item[];
+  setFilteredItems: React.Dispatch<React.SetStateAction<Item[]>>;
 };
 
-function FilterCheckbox({ onChange, defaultValue }: FilterCheckboxProps) {
+function FilterCheckbox({ itemData, setFilteredItems }: FilterCheckboxProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // searchParams object doesn't show any iterable param values, so spreading it into array gives get nested array of [key, value] query params
+  // e.g [['category', 'decks'], ['category', 'wheels']]
+  const paramsArray = [...searchParams];
+
+  // create array of only param values to be used for filtering and items state, and providing default value to checkbox component
+  // e.g ['decks', 'wheels']
+  const filterChoices: string[] = [];
+  for (let param of paramsArray) {
+    filterChoices.push(param[1]);
+  }
+
+  useEffect(() => {
+    // console.log("params array", paramsArray);
+    // console.log("filter choices", filterChoices);
+    if (filterChoices.length && itemData) {
+      // get filtered array of items with matching category
+
+      const updatedItems = itemData.filter(item =>
+        filterChoices.includes(item.category)
+      );
+      setFilteredItems(updatedItems);
+
+      return;
+    }
+
+    setFilteredItems(itemData ? itemData : []);
+  }, [filterChoices.length, itemData]);
+
   return (
     <CheckboxGroup
       colorScheme="green"
-      defaultValue={defaultValue}
-      onChange={onChange}>
+      defaultValue={filterChoices}
+      onChange={(choices: string[]) => {
+        setSearchParams({ category: choices });
+      }}>
       <Stack
         p={4}
         justifyContent="center"
         // spacing={[1, 5]}
-        direction={["column", "row"]}>
+        direction={['column', 'row']}>
         <h2>Filter by </h2>
         <Checkbox value="decks">Decks</Checkbox>
         <Checkbox value="shoes">Shoes</Checkbox>
--- client/src/pages/ShopPage/index.tsx
+++ client/src/pages/ShopPage/index.tsx
@@ -1,20 +1,14 @@
-import { Box } from '@chakra-ui/react';
 import FilterCheckbox from '../../components/inputs/FilterCheckbox.js';
+import { Item } from '../../types/ItemTypes.js';
 import { ProductCard } from './ProductCard';
 import { ProductGrid } from './ProductGrid.js';
-import { useSearchParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { Item } from '../../types/ItemTypes.js';
-
+import { useState } from 'react';
 import { useItemsQuery } from '../../hooks/useItemsQuery';
 
 function ShopPage() {
-  // const [items, setItems] = useItemsContext();
   const [filteredItems, setFilteredItems] = useState<Item[]>([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-
   // request items from api then sort returned data
-  const { isPending, isError, data: itemData, error } = useItemsQuery();
+  const { isPending, isError, data: itemData } = useItemsQuery();
 
   if (itemData)
     itemData.sort((item2, item1) => {
@@ -22,50 +16,20 @@ function ShopPage() {
       return 0;
     });
 
-  // searchParams object doesn't show any iterable param values, so spreading it into array gives get nested array of [key, value] query params
-  // e.g [['category', 'decks'], ['category', 'wheels']]
-  const paramsArray = [...searchParams];
-  // create array of only param values to be used for filtering and items state, and providing default value to checkbox component
-  // e.g ['decks', 'wheels']
-  const filterChoices: string[] = [];
-  for (let param of paramsArray) {
-    filterChoices.push(param[1]);
-  }
-
-  useEffect(() => {
-    // console.log("params array", paramsArray);
-    // console.log("filter choices", filterChoices);
-    if (filterChoices.length && itemData) {
-      // get filtered array of items with matching category
-
-      const updatedItems = itemData.filter(item =>
-        filterChoices.includes(item.category)
-      );
-      setFilteredItems(updatedItems);
-
-      return;
-    }
-
-    setFilteredItems(itemData ? itemData : []);
-  }, [filterChoices.length, itemData]);
-
   if (isPending) return <div>Loading.....</div>;
+
+  if (isError) return <div>sorry something went wrong...</div>;
 
   return (
     <div>
-      <FilterCheckbox
-        defaultValue={filterChoices}
-        onChange={(choices: string[]) => {
-          setSearchParams({ category: choices });
-        }}
-      />
-      {filteredItems.length ? (
+      <FilterCheckbox setFilteredItems={setFilteredItems} itemData={itemData} />
+      {filteredItems.length && (
         <ProductGrid>
           {filteredItems.map(item => (
             <ProductCard key={item.id} item={item} />
           ))}
         </ProductGrid>
-      ) : null}
+      )}
     </div>
   );
 }
