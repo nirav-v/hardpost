a66df57c2a52c1a4167e2cd5482ac34249e31771
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-17 13:13:14 -0800

    add to seed data

--- server/seeds.js
+++ server/seeds.js
@@ -19,19 +19,34 @@ const userData = [
     email: 'B@mail.com',
     password: 'password',
   },
+  {
+    username: 'tony_H',
+    email: 'tony@mail.com',
+    password: 'password',
+  },
 ];
 
 const itemData = [
   {
     name: 'Limosine - Cyrus Bennet board',
     category: 'decks',
-    price: 20,
+    price: 15,
     description:
       'Only skated for a couple days, has a small chip at bottom of tail',
     imagePath:
-      'https://public-hardpost-bucket.s3.amazonaws.com/limo-cyrus.jpg425e4ca14a84cea7eded38f850f06a35fd2cf4ac26bcd88ae7d1a9539d638657',
+      'https://public-hardpost-bucket.s3.amazonaws.com/limo-cyrus.jpgee7eaccd026289d83e637253bf86c91983f1ac80cae84ebc7bd9a73115c6c3f0',
     sold: false,
     userId: 2,
+  },
+  {
+    name: 'Thunder trucks',
+    category: 'trucks',
+    price: 15,
+    description: 'size 148 for 8.25 board',
+    imagePath:
+      'https://public-hardpost-bucket.s3.amazonaws.com/IMG_6972.jpegf03ad37529961436179e14160259208c9b09b1d699420797600d1d8d06d1a63b',
+    sold: false,
+    userId: 1,
   },
   {
     name: 'Converse - Fastbreak Sage colorway',
@@ -50,7 +65,7 @@ const itemData = [
     price: 30,
     description: '54mm, Formula 4 Conical Full Shape',
     imagePath:
-      'https://public-hardpost-bucket.s3.amazonaws.com/spitfiref4wheels.jpg85991e3e6b98ece6f265b503541d54f0c3b7663d6274d9946abc4c2dc9eda664',
+      'https://public-hardpost-bucket.s3.amazonaws.com/spitfiref4wheels.jpgc72db95f4bccab00dc7e37d8304891d3b2f87da197261c704e6b8687e778b288',
     sold: false,
     userId: 1,
   },
@@ -62,17 +77,27 @@ const itemData = [
     imagePath:
       'https://public-hardpost-bucket.s3.amazonaws.com/numbersMilesSilvasdeck.jpg8a890bd3118e5751ad197dbe78e5665a381cca106ecd06f8d04c82cacb131322',
     sold: false,
-    userId: 2,
+    userId: 3,
   },
   {
     name: 'Real - Mason Silva deck',
     category: 'decks',
-    price: 10,
+    price: 15,
     description: 'Moderate wear, no chips or razor tail',
     imagePath:
-      'https://public-hardpost-bucket.s3.amazonaws.com/real-mason-deck.jpg26bf5193dc1eecebcd2ea130658a2d0d0711734de97b7b9b5d8395ec5ba2cfda',
+      'https://public-hardpost-bucket.s3.amazonaws.com/real-mason-deck.jpga8e313f43991c035daa2873b0b7e9b0052fc980ab42fad8e2da0c7657cb3a31c',
     sold: false,
-    userId: 2,
+    userId: 1,
+  },
+  {
+    name: 'Butter Goods corduroy pants',
+    category: 'pants',
+    price: 35,
+    description: '32" waist, very baggy fit',
+    imagePath:
+      'https://public-hardpost-bucket.s3.amazonaws.com/IMG_6902+2.JPGbe69507a69b812c567dbd91fce0674c1402bf4979d86442cc619352ca2d25341',
+    sold: false,
+    userId: 3,
   },
 ];
 
